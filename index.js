cf690cbf75db4471deb699995875878adadf2f8e
jael-dev <jaelngaleu@gmail.com>
2023-12-06 16:14:09 +0100

    function created

---
+++ index.js
@@ -0,0 +1,42 @@
+// functions/index.js
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+
+admin.initializeApp();
+
+const app = express();
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('image'), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).send('No file uploaded.');
+    }
+
+    const filename = `${Date.now()}_${path.basename(req.file.originalname)}`;
+    const filePath = path.join(os.tmpdir(), filename);
+    fs.writeFileSync(filePath, req.file.buffer);
+
+    const storageBucket = admin.storage().bucket();
+    await storageBucket.upload(filePath, {
+      destination: `images/${filename}`,
+    });
+
+    const file = storageBucket.file(`images/${filename}`);
+    const [metadata] = await file.getMetadata();
+    const downloadURL = metadata.mediaLink;
+
+    res.status(200).json({ downloadURL });
+  } catch (error) {
+    console.error('Error uploading file:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+exports.uploadImage = functions.https.onRequest(app);
