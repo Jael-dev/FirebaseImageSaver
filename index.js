796f3221bf8353447835bf1aaa421756117de282
jael-dev <jaelngaleu@gmail.com>
2024-01-13 20:01:44 +0100

    project completed

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
-
+// functions/index.js
+const { initializeApp } = require("firebase/app");
+const admin = require('firebase-admin');
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const os = require('os');
 const fs = require('fs');
 
-admin.initializeApp();
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBSrvugxQrHCpKw6Q7vbJeKD9h5HwAP1ho",
+  authDomain: "contact-adcf7.firebaseapp.com",
+  projectId: "contact-adcf7",
+  storageBucket: "contact-adcf7.appspot.com",
+  messagingSenderId: "858093416934",
+  appId: "1:858093416934:web:07a4a90cd0ed6423226627",
+  measurementId: "G-64SJ24HW4B"
+};
+const serviceAccount = require('./contact-adcf7-firebase-adminsdk-76eum-6cea42ca4d.json');
 
-const app = express();
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: 'gs://contact-adcf7.appspot.com',
+});
+
+const app = express();  // Declare app once
+
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-app.post('/upload', upload.single('image'), async (req, res) => {
+const uploadImageHandler = async (req, res) => {
   try {
     if (!req.file) {
-      return res.status(400).send('No file uploaded.');
+      return res.status(400).json({ error: 'No file uploaded.' });
     }
 
     const filename = `${Date.now()}_${path.basename(req.file.originalname)}`;
@@ -33,8 +56,15 @@ app.post('/upload', upload.single('image'), async (req, res) => {
     res.status(200).json({ downloadURL });
   } catch (error) {
     console.error('Error uploading file:', error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).json({ error: 'Internal Server Error' });
   }
+};
+
+app.post('/upload', upload.single('image'), uploadImageHandler);
+
+const PORT = 3000;  // Use the environment variable PORT if defined, or default to 3000
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
 });
 
-exports.uploadImage = functions.https.onRequest(app);
+module.exports = app;
